5368c8a9d14c10523359dde0d1d6dcada51ee402
Sven Haardiek <Sven@Haardiek.de>
2016-09-29 00:24:06 +0200

    initial commit
    * file upload rudimentary working

---
+++ configuration.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"io/ioutil"
+	"launchpad.net/goyaml"
+	"log"
+)
+
+const (
+	CONFIG_FILE = "config"
+	ADDRESS     = "127.0.0.1:8000"
+	STATIC      = "ui"
+	DIR         = "cache"
+	TEMPDIR     = "/tmp/cache"
+)
+
+type Configuration struct {
+	Address string `yaml:"adress"`
+	Static  string `yaml:"static"`
+	Dir     string `yaml:"dir"`
+	Tempdir string `yam:"tempdir"`
+}
+
+func readConfiguration() Configuration {
+	config := Configuration{ADDRESS, STATIC, DIR, TEMPDIR}
+	content, err := ioutil.ReadFile(CONFIG_FILE)
+	if err != nil {
+		log.Println("Cannot open configuration file:", err)
+		return config
+	}
+	err = goyaml.Unmarshal(content, &config)
+	if err != nil {
+		log.Println("Cannot parse configuration file:", err)
+	}
+	return config
+}
---
+++ main.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"crypto/sha512"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"path"
+)
+
+import (
+	"github.com/gorilla/mux"
+)
+
+type HttpReturn struct {
+	Error string
+}
+
+var configuration Configuration
+
+func upload(w http.ResponseWriter, r *http.Request) {
+	probableHash := mux.Vars(r)["hash"]
+	tempfile := path.Join(configuration.Tempdir, probableHash)
+
+	out, err := os.OpenFile(tempfile, os.O_RDWR|os.O_CREATE|os.O_EXCL, 0666)
+
+	if err != nil {
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusConflict)
+		ret, err := json.Marshal(HttpReturn{"Upload in Progress"})
+		if err != nil {
+			panic(err)
+		}
+		w.Write(ret)
+		return
+
+	}
+	defer out.Close()
+	defer os.Remove(tempfile)
+
+	_, err = io.Copy(out, r.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	// Check Hash
+	hasher := sha512.New()
+	_, err = out.Seek(0, os.SEEK_SET)
+	if err != nil {
+		panic(err)
+	}
+	_, err = io.Copy(hasher, out)
+	if err != nil {
+		panic(err)
+	}
+
+	generatedHash := fmt.Sprintf("%x", hasher.Sum(nil))
+	if probableHash != generatedHash {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		ret, err := json.Marshal(HttpReturn{"Wrong hash"})
+		if err != nil {
+			panic(err)
+		}
+		w.Write(ret)
+		return
+	}
+
+	err = os.Rename(tempfile, path.Join(configuration.Dir, generatedHash))
+	if err != nil {
+		panic(err)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	ret, err := json.Marshal(HttpReturn{})
+	if err != nil {
+		panic(err)
+	}
+	w.Write(ret)
+}
+
+func routing() *mux.Router {
+	router := mux.NewRouter()
+
+	api := router.PathPrefix("/api/").Subrouter()
+	api.PathPrefix("/").Handler(http.FileServer(http.Dir(configuration.Dir))).Methods("GET", "HEAD")
+	api.HandleFunc("/{hash:[0-9|a-f]{128}}/", upload).Methods("PUT")
+
+	router.PathPrefix("/ui/").
+		Handler(http.StripPrefix("/ui/", http.FileServer(http.Dir("ui")))).
+		Methods("GET")
+
+	router.PathPrefix("/{hash:[0-9|a-f]{128}}/").
+		Handler(http.StripPrefix("/ui/", http.FileServer(http.Dir("ui")))).
+		Methods("GET")
+
+	router.PathPrefix("/").Handler(http.FileServer(http.Dir("ui"))).Methods("GET")
+
+	return router
+}
+
+func main() {
+
+	configuration = readConfiguration()
+	log.Println("Static from", configuration.Address)
+	log.Println("Serving files from", configuration.Address)
+	log.Println("Listen on", configuration.Address)
+
+	log.Fatal(http.ListenAndServe(configuration.Address, routing()))
+}
