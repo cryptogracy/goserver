7f94e870f817bdd633c04f249b5c2e2e912c158b
Sven Haardiek <Sven@Haardiek.de>
2016-10-03 16:44:28 +0200

    Add more test for data.go

--- data_test.go
+++ data_test.go
@@ -9,10 +9,46 @@ func TestDBInit(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	}
+	defer db.Close()
 
 	var res string
 	err = db.db.QueryRow("SELECT name FROM sqlite_master WHERE type='table' AND name='Files'").Scan(&res)
 	if err != nil {
 		t.Error(err)
 	}
+}
+
+/* I do not like this test, cause adding and checking are tested in the same
+ * function...maybe both are broken */
+func TestAddandCheck(t *testing.T) {
+	db, _ := DBInit(":memory:")
+	defer db.Close()
+
+  // Test adding
+	if err := db.Add("Hash1", 10); err != nil { t.Error(err)}
+	if err := db.Add("Hash1", 2); err == nil { t.Error(err)}
+	if err := db.Add("Hash2", -5); err != nil { t.Error(err)}
+
+  // Test checking and proof if adding has worked.
+  if !db.Check("Hash1") { t.Error("Hash1 not present")}
+  if !db.Check("Hash2") { t.Error("Hash2 not present")}
+  if db.Check("Hash3") { t.Error("Hash3 present")}
+}
+
+func TestCleanup(t *testing.T) {
+	db, _ := DBInit(":memory:")
+	defer db.Close()
+
+  // Add some Data
+	db.Add("Hash1", 10)
+	db.Add("Hash2", 0)
+	db.Add("Hash3", -5)
+
+  affected, err := db.Cleanup()
+
+  if err != nil { t.Error(err) }
+  if affected != 2 { t.Error("Wrong number of deleted Rows") }
+  if !db.Check("Hash1") { t.Error("Hash1 not present")}
+  if db.Check("Hash2") { t.Error("Hash2 present")}
+  if db.Check("Hash3") { t.Error("Hash3 present")}
 }
