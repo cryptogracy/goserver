12415935fa6a7302c000ffa5339d05de590e6bad
Sven Haardiek <Sven@Haardiek.de>
2016-10-03 21:50:24 +0200

    Update routing to match new documentation

--- route.go
+++ route.go
@@ -3,28 +3,45 @@ package main
 import (
 	"github.com/gorilla/mux"
 	"net/http"
+	"path"
 )
 
 func routing() *mux.Router {
 	router := mux.NewRouter()
 
-	// Routes for API
+	// Get upload ui from /
+	router.Path("/").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, path.Join(configuration.Static, "upload.html"))
+	}).		Methods("GET")
+
+	// Get download ui to download file with hash <hash> from /<hash>
+	router.Path("/{hash:[0-9|a-f]{128}}").HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			http.ServeFile(w, r, path.Join(configuration.Static, "download.html"))
+		}).Methods("GET")
+
+	// Get all files from configuration.Static from /<configuration.Static>
+	router.PathPrefix(path.Join("/", configuration.Static)).
+		Handler(http.StripPrefix(path.Join("/", configuration.Static),
+		http.FileServer(http.Dir(configuration.Static)))).Methods("GET")
+
+	// Subrouter for /api/
 	api := router.PathPrefix("/api/").Subrouter()
-	api.PathPrefix("/").Handler(http.StripPrefix("/api/", http.FileServer(http.Dir(configuration.Dir)))).Methods("GET")
-	api.HandleFunc("/{hash:[0-9|a-f]{128}}", Upload).Methods("PUT").HeadersRegexp("x-http-lifespan", "[0-9]+")
 
-	// Routes for ui upload
-	router.PathPrefix("/ui/").
-		Handler(http.StripPrefix("/ui/", http.FileServer(http.Dir("ui")))).
-		Methods("GET")
+	api.Path("/").Handler(http.StripPrefix("/api/", http.FileServer(
+		http.Dir(configuration.Dir)))).Methods("GET")
 
-	// Routes for ui download
-	router.PathPrefix("/{hash:[0-9|a-f]{128}}/").
-		Handler(http.StripPrefix("/ui/", http.FileServer(http.Dir("ui")))).
-		Methods("GET")
+	// Put the file (with the lifespan in the header) to /api/<hash>
+	api.HandleFunc("/{hash:[0-9|a-f]{128}}", Upload).Methods("PUT").
+		HeadersRegexp("x-http-lifespan", "[0-9]+")
 
-	// Deliver upload ui also under /
-	router.PathPrefix("/").Handler(http.FileServer(http.Dir("ui"))).Methods("GET")
+	// Push the metadata (with the lifespan in the header) to /api/metadata
+	api.HandleFunc("/metadata", func(w http.ResponseWriter, r *http.Request) {}).
+		Methods("PUSH")
+
+	// Get the metadata with the <id> from /api/metadata/<id>
+	api.HandleFunc("/metadata/[[:alnum:]]",
+		func(w http.ResponseWriter, r *http.Request) {}).Methods("GET")
 
 	return router
 }
