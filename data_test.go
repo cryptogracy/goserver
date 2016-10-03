b23ddfa646f81aa30102335c81d65e92369e12c2
Sven Haardiek <Sven@Haardiek.de>
2016-10-03 12:14:04 +0200

    fmt

--- data_test.go
+++ data_test.go
@@ -1,14 +1,18 @@
 package main
 
 import (
-  "testing"
+	"testing"
 )
 
 func TestDBInit(t *testing.T) {
-  db, err := DBInit(":memory:")
-  if err != nil { t.Error(err)}
+	db, err := DBInit(":memory:")
+	if err != nil {
+		t.Error(err)
+	}
 
 	var res string
 	err = db.db.QueryRow("SELECT name FROM sqlite_master WHERE type='table' AND name='Files'").Scan(&res)
-  if err != nil { t.Error(err) }
+	if err != nil {
+		t.Error(err)
+	}
 }
